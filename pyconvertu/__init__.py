c903bd3a127e5e7f4ebb36770ffd5259a4a291b1
The Economist <econcz@users.noreply.github.com>
2021-05-29 02:19:16 +0200

    pyconvertu
    Tools for creation or conversion of lists from/to desired classification (the default is ISO 3166-1)

--- pyconvertu/__init__.py
+++ pyconvertu/__init__.py
@@ -1,8 +1,3 @@
-import pkgutil
-
-# ISO-3166-1 JSON file
-pyconvertu_classification = pkgutil.get_data(__name__, r'classification.json')
-
 from pyconvertu.pyconvertu import convert              # main function
-from pyconvertu.pyconvertu import classification       # full classification
+from pyconvertu.pyconvertu import classification       # classification
 from pyconvertu.pyconvertu import info                 # info, metadata
